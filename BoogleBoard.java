e27cc2499945a97c3d26dbc7a0fdfca1f9918924
tatiana <tatiana.pekhtereva@gmail.com>
2019-08-03 21:00:39 +1200

    commiting

--- BoogleBoard.java
+++ BoogleBoard.java
@@ -6,7 +6,7 @@ public class BoogleBoard {
 		BoogleBoard boggleBoard = new BoogleBoard();
 		String booggleArr[][] = { { "D", "R", "F" }, 
 				                  { "C", "E", "G" } };
-		String word = "EG";
+		String word = "EGF";
 
 		System.out.println("Start");
 		System.out.println("end:" + checkBoogleBoard(booggleArr, word));
@@ -29,7 +29,7 @@ public class BoogleBoard {
 						letterFound = true;
 						booggleArr[x][y] = "~";
 						if (letterIndex + 1 < wordArr.length) // not the last digit of the word
-							letterFound = checkNeighbours(x, y, wordArr[letterIndex + 1], booggleArr);
+							letterFound = checkNeighbours(x, y, booggleArr, wordArr, letterIndex+1 );
 					}
 				}
 
@@ -38,39 +38,64 @@ public class BoogleBoard {
 		return letterFound;
 	}
 
-	public static boolean checkNeighbours(int x, int y, char letter, String[][] booggleArr) {
+	public static boolean checkNeighbours(int x, int y, String[][] booggleArr, char[] wordArr, int letterIndex ) {
+		boolean neighbourFound = false;
+		char letter = wordArr[letterIndex];
+		System.out.println("letterIndex="+letterIndex+" wordArr= "+ wordArr.toString());
 		System.out.println("x=" + x + "  y=" + y + " letter=" + letter);
+		
 		for (int x1 = 0; x1 < booggleArr.length; x1++) {
 			for (int y1 = 0; y1 < booggleArr[x1].length; y1++) {
 				System.out.print(booggleArr[x1][y1] + ", ");
 			}
 			System.out.println();
 		}
+		
 		if(x + 1 < booggleArr.length ) System.out.println("Checking neighbour 1: "+ booggleArr[x + 1][y]);
 		if (x + 1 < booggleArr.length && booggleArr[x + 1][y].equals(Character.toString(letter))) {
 			System.out.println("found neigbour: " + booggleArr[x + 1][y]);
-			return true;
+			booggleArr[x + 1][y] = "~";
+			neighbourFound = true;
+			if (letterIndex + 1 < wordArr.length) // not the last digit of the word
+			  checkNeighbours(x + 1, y, booggleArr,wordArr, letterIndex+1);
+			else
+				return false;
 		}
 		
 		if(x > 0) System.out.println("Checking neighbour 2: "+ booggleArr[x-1][y]);
 		if (x > 0 && booggleArr[x - 1][y].equals(Character.toString(letter))) {
 			System.out.println("found neigbour: " + booggleArr[x - 1][y]);
-			return true;
+			booggleArr[x - 1][y] = "~";
+			neighbourFound = true;
+			if (letterIndex + 1 < wordArr.length) // not the last digit of the word
+			  checkNeighbours(x - 1, y, booggleArr,wordArr, letterIndex+1);
+			else 
+				return false;
 		}
 		
 		if(y + 1 < booggleArr[x].length) System.out.println("Checking neighbour 3: "+ booggleArr[x][y + 1]);
 		if (y + 1 < booggleArr[x].length && booggleArr[x][y + 1].equals(Character.toString(letter))) {
 			System.out.println("found neigbour: " + booggleArr[x][y + 1]);
-			return true;
+			booggleArr[x][y + 1] = "~";
+			neighbourFound = true;
+			if (letterIndex + 1 < wordArr.length) // not the last digit of the word
+			  checkNeighbours(x, y + 1, booggleArr,wordArr, letterIndex+1);
+			else
+				return false;
 		}
 		
 		if(y > 0 )	System.out.println("Checking neighbour 4: "+ booggleArr[x][y - 1]);
 		if (y > 0 && booggleArr[x][y - 1].equals(Character.toString(letter))) {
 			System.out.println("found neigbour: " + booggleArr[x][y - 1]);
-			return true;
+			booggleArr[x][y - 1] = "~";
+			neighbourFound = true;
+			if (letterIndex + 1 < wordArr.length) // not the last digit of the word
+			  checkNeighbours(x,y - 1, booggleArr,wordArr, letterIndex+1);
+			else
+				return false;					
 		}
 
-		return false;
+		return neighbourFound;
 
 	}
 
